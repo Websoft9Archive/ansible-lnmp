cc6afa4a36d4c4bdc99197859b5f6c3c8de58c22
unknown <xu_junkai@foxmail.com>
2018-12-06 15:17:15 +0800

    update

--- roles/mysql/files/getpass.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pass=$(grep "A temporary password is generated for root@localhost:" /var/log/messages |awk '{print $16}')
-
-cat > /root/.my.cnf <<EOF
-[client]
-user=root
-password=$pass
-EOF
