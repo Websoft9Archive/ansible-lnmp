8e2524e0aea9ad7e361508fb62104116b9f0357b
Mozlk <xu_junkai@foxmail.com>
2019-06-21 08:57:59 +0800

    kk

--- roles/mysql/files/init-57.sh
+++ roles/mysql/files/init-57.sh
@@ -6,7 +6,7 @@ new_password=$(</dev/urandom tr -dc '12345!#$%^&*()qwertQWERTasdfgASDFGzxcvbZXCV
 systemctl restart mysqld
 mysqladmin -uroot -p${old_password} -h localhost password $new_password
 
-echo -e 'MySQL username:root\nDatabases root Password:'$new_password  > /credentials/password.txt
+echo -e 'MySQL username:root\nMySQL Password:'$new_password  > /credentials/password.txt
 
 
 sed -i "s/\/root\/init.sh//" /etc/rc.local
